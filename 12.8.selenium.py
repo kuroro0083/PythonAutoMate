9858e65c4629bd6bef986c177b37660f21231ab3
killua0083 <killua0083@qq.com>
2024-06-27 22:46:27 +0800

    add some ex

---
+++ 12.8.selenium.py
@@ -0,0 +1,7 @@
+# openssl 在py37环境下显示是不知道为什么变成1.1.0t版本
+# from selenium import webdriver
+import ssl
+print(ssl.OPENSSL_VERSION)
+
+# browser = webdriver.Edge()
+# browser.get('https://www.bing.com')
